4ae3a4abd394a44d037a51002df6ec2a815f3a7a
unknown <markothedevmail@gmail.com>
2022-06-11 00:17:20 +0100

    added CSRF_TRUSTED_ORIGINS to settig to handle csrf error

--- core/settings.py
+++ core/settings.py
@@ -130,6 +130,7 @@ STATIC_ROOT =os.path.join(BASE_DIR,'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_COOKIE_SECURE =True
+# CSRF_COOKIE_SECURE =True
+CSRF_TRUSTED_ORIGINS = ['https://www.discordchatterscommunity.org']
 
 django_heroku.settings(config=locals(), staticfiles=False,logging=False)
